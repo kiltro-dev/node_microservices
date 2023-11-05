3a44d5d17301e0a3ccbc4b9d04259fff979e67a5
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-11-05 13:18:00 -0300

    customers instead clients

--- customers/prisma/migrations/20231105161406_create_customer/migration.sql
+++ customers/prisma/migrations/20231105161406_create_customer/migration.sql
@@ -1,13 +1,13 @@
 -- CreateTable
-CREATE TABLE "clients" (
+CREATE TABLE "customers" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
 
-    CONSTRAINT "clients_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "clients_email_key" ON "clients"("email");
+CREATE UNIQUE INDEX "customers_email_key" ON "customers"("email");
